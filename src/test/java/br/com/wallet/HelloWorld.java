3d0e087160d52c644125b73ecff72be01e1021c2
Benucci <vinibenucci@gmail.com>
2021-03-11 14:41:31 -0300

    Conigs. iniciais p os testes

--- src/test/java/br/com/wallet/HelloWorld.java
+++ src/test/java/br/com/wallet/HelloWorld.java
@@ -1,18 +1,21 @@
 package br.com.wallet;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
 @RunWith(SpringRunner.class)
-@ActiveProfiles("test")
-class WalletApplicationTests {
+@SpringBootTest
+public class HelloWorld {
 
 	@Test
-	void contextLoads() {
+	public void testHelloWorld() {
+
+		assertEquals(1, 1);
+
 	}
 
 }
